7f27e12b0bfd567aa43d5e5c9866588f7af78c5b
kc648 <kc648@njit.edu>
2025-11-12 14:28:21 -0500

    Added standalone components and updated routing

---
+++ mean-stack-frontend/src/app/root/root.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './root.html',
+  styleUrl: './root.css',
+})
+export class Root {
+
+}
