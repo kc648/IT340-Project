c8fb31aeb3686f0ac71b49300badbdcda79aa113
kc648 <kc648@njit.edu>
2025-11-12 15:35:54 -0500

    Configure landing and login page routes

--- mean-stack-frontend/src/app/app.routes.ts
+++ mean-stack-frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from ./landing/landing.component';
-import {LoginComponent } from ./login/login.component';
+import { Landing } from './landing/landing';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-	{ path: '', component: LandingComponent },
-	{ path: 'login', component: LoginComponent },
+	{ path: '', component: Landing },
+	{ path: 'login', component: Login },
 ];
--- mean-stack-frontend/src/app/landing/landing.ts
+++ mean-stack-frontend/src/app/landing/landing.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
