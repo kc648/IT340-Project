50f076ae0d825ec247bb56c02b11b1288828dad3
kc648 <kc648@njit.edu>
2025-11-12 20:49:37 -0500

    Added dropdown for Features in nav bar

--- mean-stack-frontend/src/app/app.routes.ts
+++ mean-stack-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Landing } from './landing/landing';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Calendar } from './calendar/calendar';
 
 export const routes: Routes = [
 	{ path: '', component: Landing },
 	{ path: 'login', component: Login },
 	{ path: 'register', component: Register },
+	{ path: 'calendar', component: Calendar },
 ];
---
+++ mean-stack-frontend/src/app/calendar/calendar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-calendar',
+  imports: [RouterModule],
+  templateUrl: './calendar.html',
+  styleUrl: './calendar.css',
+})
+export class Calendar {
+
+}
