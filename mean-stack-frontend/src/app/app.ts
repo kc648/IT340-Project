7e195533aa87e40092d200bccd36d7f520447e26
kc648 <kc648@njit.edu>
2025-12-13 18:50:14 -0500

    Revert

--- mean-stack-frontend/src/app/app.ts
+++ mean-stack-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
