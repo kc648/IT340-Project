64d94d3eb285d0859b62e134ec17267fff02aaf0
kc648 <kc648@njit.edu>
2025-12-13 14:32:47 -0500

    Frontend input pulled and fed to MongoDB

--- mean-stack-frontend/src/main.ts
+++ mean-stack-frontend/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { App } from './app/app';
+import { routes } from './app/app.routes';
+import { authInterceptor } from './app/auth.interceptor'; // ✅ ADD THIS
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor]) // ✅ ADD THIS
+    )
+  ]
+});
+
+
