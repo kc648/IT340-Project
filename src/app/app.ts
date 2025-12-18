7e30a7f8ffe504dcde88985b269d3c69cad5000c
kc648 <kc648@njit.edu>
2025-12-18 16:22:41 -0500

    Tweaked calendar styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { FormsModule } from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, FormsModule, FullCalendarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
